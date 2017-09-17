6ff29d0272c0585c924a804a4dcc7d0b7a8fbdb0
cooljack0907 <31000157+cooljack0907@users.noreply.github.com>
2017-09-17 19:47:05 +0800

    完成圖鑑textview

--- app/src/main/java/es/esy/mobilehost/android/savelife/GalleryDetail.java
+++ app/src/main/java/es/esy/mobilehost/android/savelife/GalleryDetail.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GalleryDetail extends AppCompatActivity {
     ImageView selectedImage;
+    TextView selectedText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +17,8 @@ public class GalleryDetail extends AppCompatActivity {
         selectedImage = (ImageView) findViewById(R.id.selectedImage); // init a ImageView
         Intent intent = getIntent(); // get Intent which we set from Previous Activity
         selectedImage.setImageResource(intent.getIntExtra("image", 0)); // get image from Intent and set it in ImageView
+        selectedText =(TextView) findViewById(R.id.selectedText);
+        selectedText.setText(intent.getIntExtra("text",0));
+
     }
 }
