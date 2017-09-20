40835327ae7d39e69b15e0773ed443a11b0c5682
is77 <holygrailh@yahoo.com.tw>
2017-09-20 16:01:20 +0800

    新增背景音量調整 與按鍵音量調整功能

--- app/src/main/java/es/esy/mobilehost/android/savelife/SetupActivity.java
+++ app/src/main/java/es/esy/mobilehost/android/savelife/SetupActivity.java
@@ -5,21 +5,37 @@ import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
+import android.widget.Toast;
+
 
 public class SetupActivity extends Activity {
 
     private Context context;
+    private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
+    private SeekBar bgControl, seVolControl;
+    private SoundPool soundPool;
+    private int sound01,sound02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
         context = this;
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(context, R.raw.nothing_on_you);
+
+        soundPool = new SoundPool(10, AudioManager.STREAM_SYSTEM,5);
+        sound01 = soundPool.load(SetupActivity.this, R.raw.sound01, 0);
     }
 
     // Button click 事件回呼方法
@@ -30,11 +46,27 @@ public class SetupActivity extends Activity {
 //                showDialog_2();
 //                break;
 
-            case R.id.button6: // 遊戲設定
+            case R.id.button6: // 版權說明
                 copyRightView();
                 break;
 
-            case R.id.button7: // 版權說明
+            case R.id.button7: // 遊戲設定
+                try {
+                    if (mediaPlayer != null) {
+                        mediaPlayer.stop();
+                    }
+                    mediaPlayer.prepare();
+
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+
+                            mediaPlayer.start();
+                          }
+                    });
+                } catch (Exception e) {
+                    Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show();
+                }
                 gameSetUp();
                 break;
         }
@@ -113,26 +145,58 @@ public class SetupActivity extends Activity {
             }
         });
 
-        builder.setNegativeButton(R.string.cancel, new OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
+//        builder.setNegativeButton(R.string.cancel, new OnClickListener() {
+//
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                dialog.dismiss();
+//            }
+//        });
         builder.create().show();
     }
 
     // 音量設定 View
-    private void soundVolume() {
+    public void soundVolume() {
+//        mediaPlayer.prepare();
+//
+//        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(MediaPlayer mp) {
+//
+//                mediaPlayer.start();
+//                // 同步到播放進度函式
+//                new DelayThread().start();
+//            }
+//        });
+
         // 自定Layout
         LayoutInflater inflater = getLayoutInflater();
         // 將 xml layout 轉換成視圖 View 物件
         View layout = inflater.inflate(R.layout.dialog,
-                (ViewGroup) findViewById(R.id.volume));
+                (ViewGroup) findViewById(R.id.menu_volume));
         // 自定View
-        SeekBar S1 = (SeekBar) findViewById(R.id.seekBar);
-        SeekBar S2 = (SeekBar) findViewById(R.id.seekBar2);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxBgVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curBgVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        int maxSeVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_SYSTEM);
+        int curSeVolume = audioManager.getStreamVolume(AudioManager.STREAM_SYSTEM);
+
+        // 控制聲音大小
+        bgControl = (SeekBar) layout.findViewById(R.id.seekBar);
+        bgControl.setMax(maxBgVolume);
+        bgControl.setProgress(curBgVolume);
+        bgControl.setOnSeekBarChangeListener(new MyVolControlOnSeekBarChangeListener());
+
+        //控制音效大小
+        seVolControl = (SeekBar) layout.findViewById(R.id.seekBar2);
+        seVolControl.setMax(maxSeVolume);
+        seVolControl.setProgress(curSeVolume);
+        seVolControl.setOnSeekBarChangeListener(new MyVolControlOnSeekBarChangeListener());
+
+//        SeekBar S1 = (SeekBar) findViewById(R.id.seekBar);
+//        SeekBar S2 = (SeekBar) findViewById(R.id.seekBar2);
 
         Builder builder = new Builder(context);
         builder.setTitle(R.string.button_text7);
@@ -187,7 +251,7 @@ public class SetupActivity extends Activity {
         LayoutInflater inflater = getLayoutInflater();
         // 將 xml layout 轉換成視圖 View 物件
         View layout = inflater.inflate(R.layout.datainfo,
-                (ViewGroup) findViewById(R.id.dataresource));
+                (ViewGroup) findViewById(R.id.menu_copyright));
         Builder builder = new Builder(context);
         builder.setTitle("資料出處");
         builder.setIcon(android.R.drawable.ic_dialog_info);
@@ -213,7 +277,7 @@ public class SetupActivity extends Activity {
         LayoutInflater inflater = getLayoutInflater();
         // 將 xml layout 轉換成視圖 View 物件
         View layout = inflater.inflate(R.layout.teamwork,
-                (ViewGroup) findViewById(R.id.dataresource));
+                (ViewGroup) findViewById(R.id.menu_copyright));
         Builder builder = new Builder(context);
         builder.setTitle("製作團隊");
         builder.setIcon(android.R.drawable.ic_dialog_info);
@@ -233,4 +297,33 @@ public class SetupActivity extends Activity {
         });
         builder.create().show();
     }
+
+    // 調整聲音大小
+    private class MyVolControlOnSeekBarChangeListener implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+            switch (seekBar.getId()){
+                case R.id.seekBar :
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                    break;
+                // 改變右聲道音量
+                case R.id.seekBar2 :
+                    audioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, progress, 0);
+                    float vol = progress/1F;
+                    soundPool.setVolume(sound01, vol, vol);
+                    soundPool.play(sound01, vol, vol, 0, 0, 1);
+                    break;
+            }
+        }
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+        }
+    }
+
+
+
 }
