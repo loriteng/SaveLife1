8d0300140b17d7ab2144226b9b60b5a626b51364
loriteng <loriteng@gmail.com>
2017-09-22 09:27:03 +0800

    登入後撥放背景音樂OK

--- app/src/main/java/es/esy/mobilehost/android/savelife/HomeActivity.java
+++ app/src/main/java/es/esy/mobilehost/android/savelife/HomeActivity.java
@@ -1,6 +1,7 @@
 package es.esy.mobilehost.android.savelife;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -12,10 +13,22 @@ import android.widget.Toast;
 
 public class HomeActivity extends MenuActivity {
 
+    private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //背景音樂播放
+        mediaPlayer = MediaPlayer.create(this, R.raw.nothing_on_you);
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mediaPlayer.start();
+                }
+        });
+
 
         //進入遊戲
         Button hometoplaygame = (Button) findViewById(R.id.BStart);
